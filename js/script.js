a74c8f6229c2c48ae3e57a37b2a9a1a9757b33e5
VolodymyrRomanytsia <romanyca@gmail.com>
2018-08-19 14:47:27 +0300

    Fixed JS

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-
-var button = document.querySelector('.button');
-button.onclick = function() {
-	var number = document.getElementById('inputNum').value;
-	var sum = ' ';
-	var sharp = '#';
-	for (var i = 0; i < number; i++) {
+const button = document.querySelector('.button');
+button.onclick = () => {
+	let number = document.getElementById('inputNum').value;
+	let sum = ' ';
+	const sharp = '#';
+	for (let i = 0; i < number; i++) {
 		sum += sharp;
-		var container = document.body.children[0];	
-		var block = container.children[2];
-		var ul = block.children[0];
-		var li = document.createElement('li');
+		const container = document.body.children[0];	
+		const block = container.children[2];
+		const ul = block.children[0];
+		let li = document.createElement('li');
 		ul.appendChild(li);
 		li.insertAdjacentHTML("afterBegin", sum);
 		console.log (sum);
